fd00bdac25cd6547342332c58efb5496b1d79d8b
root <root@localhost>
2023-06-13 23:48:51 -0400

    ya

--- discord/ext/commands/cog.py
+++ discord/ext/commands/cog.py
@@ -381,8 +381,8 @@ class Cog(metaclass=CogMeta):
 
         return self
 
-    def __str__(self): -> str:
-        return self.__cog_name__
+    #def __str__(self): -> str:
+    #    return self.__cog_name__
 
     def get_commands(self) -> List[Command[Self, ..., Any]]:
         r"""Returns the commands that are defined inside this cog.
