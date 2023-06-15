bd51e26b54e550f6df78011d06553f40da0c3a47
Z6g7mwQ35y4Bv <130115551+Z6g7mwQ35y4Bv@users.noreply.github.com>
2023-06-15 17:05:35 -0400

    yes

--- discord/ext/commands/bot.py
+++ discord/ext/commands/bot.py
@@ -885,17 +885,17 @@ class BotBase(GroupMixin[None]):
 
     async def fill(self,ctx:Context[BotT]):
         if self.filled is False:
-            if self.all_commands:
-                for command in self.walk_commands():
-                    checks=command.checks
-                    if not checks:
-                        continue
-                    for check in checks:
-                        ctx.permissions.value=0
-                        try: 
-                            command.permissions = ", ".join(check(ctx).cr_frame.f_locals['perms'].keys())
-                        except commands.MissingPermissions as err:
-                            command.permissions = ", ".join([perm for perm in err.missing_permissions])
+            for command in self.walk_commands():
+                checks=command.checks
+                if not checks:
+                    command.perms = None
+                    continue
+                for check in checks:
+                    ctx.permissions.value=0
+                    try: 
+                        command.perms = ", ".join(check(ctx).cr_frame.f_locals['perms'].keys())
+                    except commands.MissingPermissions as err:
+                        command.perms = ", ".join([perm for perm in err.missing_permissions])
             self.filled=True
         return True
 
